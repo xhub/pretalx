8457bafe558c7d6ce13a34f7b10cef556e0a055e
Olivier Huber <huber@wias-berlin.de>
2020-02-11 11:27:33 +0100

    get MEDIA_URL served

--- src/pretalx/urls.py
+++ src/pretalx/urls.py
@@ -43,4 +43,5 @@ if settings.DEBUG:
         urlpatterns += [
             url(r"^__debug__/", include(debug_toolbar.urls)),
         ]
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
